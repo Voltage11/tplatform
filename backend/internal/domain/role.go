9f866a2c5c7d72dd7e3692cab986cb92989bd2b2
Владимир <voltage2002@yandex.ru>
2026-06-30 12:03:29 +0300

    Create domain Role

---
+++ backend/internal/domain/role.go
@@ -0,0 +1,44 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// ********************* РОЛЬ *********************
+
+// Role — роль пользователя
+type Role struct {
+	ID          uuid.UUID
+	Name        string
+	Description string
+	CreatedAt   time.Time
+}
+
+type RoleDetail struct {
+	ID   uuid.UUID
+	Name string
+}
+
+type RoleFilter struct {
+	Name       string
+	Pagination PaginationRequest
+}
+
+type RoleRepository interface {
+	Create(ctx context.Context, role *Role) error
+	Update(ctx context.Context, role *Role) error
+	GetByID(ctx context.Context, id uuid.UUID) (*Role, error)
+	GetList(ctx context.Context, filter RoleFilter) ([]*Role, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type RoleService interface {
+	Create(ctx context.Context, role *Role) error
+	Update(ctx context.Context, role *Role) error
+	GetByID(ctx context.Context, id uuid.UUID) (*Role, error)
+	GetList(ctx context.Context, filter RoleFilter) ([]*Role, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
